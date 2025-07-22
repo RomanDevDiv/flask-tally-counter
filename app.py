979b35bb82988d9ddff40900236755bc0e8b75cf
RomanDevDiv <rlkolosov@gmial.com>
2025-07-22 13:32:20 +0300

    no yaml

--- app.py
+++ app.py
@@ -143,5 +143,4 @@ if __name__ == '__main__':
     print(f"Database file: {os.path.abspath(DATABASE)}")
     
     # Run the Flask app
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='127.0.0.1', port=5000)
